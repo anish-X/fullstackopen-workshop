2fd13a5822181aff19489ac2d5670ead3bb2cd24
anish-X <anish.stha527@gmail.com>
2025-07-30 20:36:01 +0545

    complete jsx

---
+++ part-1/introdemo/src/main.js
@@ -0,0 +1,8 @@
+import ReactDOM from "react-dom/client"
+import React from "react"
+import App from './App'
+
+const react = document.getElementById('root');
+const reactRoot = ReactDOM.createRoot(react);
+
+reactRoot.render(React.createElement(App))
